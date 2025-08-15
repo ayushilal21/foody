bb9a4186c52bafd6e9cb44d33f8493d8221f2b17
ayushilal21 <ayushi.lal.21@gmail.com>
2025-08-15 16:12:37 +0530

    type

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,17 @@
 import {Redirect, Slot, Tabs} from "expo-router";
+import {TabBarIconProps} from "@/type";
 import {Image, Text, View} from "react-native";
 import {images} from "@/constants";
 import cn from "clsx";
+
+const TabBarIcon = ({ focused, icon, title }: TabBarIconProps) => (
+    <View className="tab-icon">
+        <Image source={icon} className="size-7" resizeMode="contain" tintColor={focused ? '#FE8C00' : '#5D5F6D'} />
+        <Text className={cn('text-sm font-bold', focused ? 'text-primary':'text-gray-200')}>
+            {title}
+        </Text>
+    </View>
+)
 
 export default function TabLayout() {
     const isAuthenticated = true;
@@ -33,24 +43,28 @@ export default function TabLayout() {
                 name='index'
                 options={{
                     title: 'Home',
+                    tabBarIcon: ({ focused }) => <TabBarIcon title="Home" icon={images.home} focused={focused} />
                 }}
             />
             <Tabs.Screen
                 name='search'
                 options={{
                     title: 'Search',
+                    tabBarIcon: ({ focused }) => <TabBarIcon title="Search" icon={images.search} focused={focused} />
                 }}
             />
             <Tabs.Screen
                 name='cart'
                 options={{
                     title: 'Cart',
+                    tabBarIcon: ({ focused }) => <TabBarIcon title="Cart" icon={images.bag} focused={focused} />
                 }}
             />
             <Tabs.Screen
                 name='profile'
                 options={{
                     title: 'Profile',
+                    tabBarIcon: ({ focused }) => <TabBarIcon title="Profile" icon={images.person} focused={focused} />
                 }}
             />
         </Tabs>
